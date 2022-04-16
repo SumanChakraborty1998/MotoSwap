9f46703642dada0887e34af5e6f810c0a7fc80db
SumanChakraborty1998 <sumanchakra8145@gmail.com>
2022-04-16 20:10:24 +0530

    Initial Commit

---
+++ contracts/Dex.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Dex {
+    struct Token {
+        bytes32 ticker;
+        address tokenAddress;
+    }
+
+    mapping(bytes32 => Token) public tokens;
+
+    bytes32[] public tokenList;
+    address public admin;
+
+    constructor() public {
+        admin = msg.sender;
+    }
+}
