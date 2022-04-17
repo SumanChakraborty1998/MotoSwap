11a527946c1845c55aa06dde0c0de9c55919b84c
SumanChakraborty1998 <sumanchakra8145@gmail.com>
2022-04-17 22:49:23 +0530

    wallet logic is done

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract Dex {
     struct Token {
         bytes32 ticker;
@@ -12,7 +14,55 @@ contract Dex {
     bytes32[] public tokenList;
     address public admin;
 
+    mapping(address => mapping(bytes32 => uint256)) public traderBalances;
+
     constructor() public {
         admin = msg.sender;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can add new Tokens");
+        _;
+    }
+
+    modifier tokenExists(bytes32 _ticker) {
+        require(
+            tokens[_ticker].tokenAddress != address(0),
+            "Token is not supported"
+        );
+        _;
+    }
+
+    function addToken(bytes32 ticker, address tokenAddress) external onlyAdmin {
+        Token memory token = Token(ticker, tokenAddress);
+        tokens[ticker] = token;
+        tokenList.push(ticker);
+    }
+
+    function deposit(uint256 amount, bytes32 ticker)
+        external
+        tokenExists(ticker)
+    {
+        IERC20(tokens[ticker].tokenAddress).transferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
+
+        traderBalances[msg.sender][ticker] += amount;
+    }
+
+    function withdraw(uint256 amount, bytes32 ticker)
+        external
+        tokenExists(ticker)
+    {
+        require(
+            traderBalances[msg.sender][ticker] >= amount,
+            "Not enough Balances"
+        );
+
+        IERC20(tokens[ticker].tokenAddress).transfer(msg.sender, amount);
+
+        traderBalances[msg.sender][ticker] -= amount;
     }
 }
